5b4d38a0e92c874484389b7cf85708e32d9390af
DESKTOP-239NLUT\Administrator <sanjin980223@gmail.com>
2018-10-14 03:31:45 +0800

    add dateTime to personDetails

--- src/test/java/seedu/address/testutil/PersonUtil.java
+++ src/test/java/seedu/address/testutil/PersonUtil.java
@@ -1,17 +1,18 @@
 package seedu.address.testutil;
 
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DATETIME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
-
 import java.util.Set;
 
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.EditCommand;
 import seedu.address.model.calendarevent.CalendarEvent;
 import seedu.address.model.tag.Tag;
+
 
 /**
  * A utility class for CalendarEvent.
@@ -33,6 +34,7 @@ public class PersonUtil {
         sb.append(PREFIX_NAME + calendarEvent.getName().fullName + " ");
         sb.append(PREFIX_PHONE + calendarEvent.getPhone().value + " ");
         sb.append(PREFIX_EMAIL + calendarEvent.getEmail().value + " ");
+        sb.append(PREFIX_DATETIME + calendarEvent.getDateTime().localDateTime.toString() + " ");
         sb.append(PREFIX_ADDRESS + calendarEvent.getLocation().value + " ");
         calendarEvent.getTags().stream().forEach(
             s -> sb.append(PREFIX_TAG + s.tagName + " ")
@@ -48,6 +50,7 @@ public class PersonUtil {
         descriptor.getName().ifPresent(name -> sb.append(PREFIX_NAME).append(name.fullName).append(" "));
         descriptor.getPhone().ifPresent(phone -> sb.append(PREFIX_PHONE).append(phone.value).append(" "));
         descriptor.getEmail().ifPresent(email -> sb.append(PREFIX_EMAIL).append(email.value).append(" "));
+        descriptor.getDateTime().ifPresent(dateTime -> sb.append(PREFIX_DATETIME).append(dateTime.localDateTime).append(" "));
         descriptor.getLocation().ifPresent(address -> sb.append(PREFIX_ADDRESS).append(address.value).append(" "));
         if (descriptor.getTags().isPresent()) {
             Set<Tag> tags = descriptor.getTags().get();
