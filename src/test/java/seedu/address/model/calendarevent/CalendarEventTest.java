ca677554b226d1312004b7bb434c6dc78a80ae19
DESKTOP-239NLUT\Administrator <sanjin980223@gmail.com>
2018-10-14 00:09:08 +0800

    change the CalendarEventTest and ca pass now

--- src/test/java/seedu/address/model/calendarevent/CalendarEventTest.java
+++ src/test/java/seedu/address/model/calendarevent/CalendarEventTest.java
@@ -2,7 +2,12 @@ package seedu.address.model.calendarevent;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static seedu.address.logic.commands.CommandTestUtil.*;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_DATETIME_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.BOB;
 
